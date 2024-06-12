acebdd636c2f450316bb08da6e381e90b51892b0
mmadiee <149312891+mmadiee@users.noreply.github.com>
2024-06-12 23:01:04 +0800

    updated and finished the js logic of the game. UI is working as intended.

--- rps.js
+++ rps.js
@@ -1,17 +1,17 @@
 let humanScore = 0;
 let computerScore = 0;
+let round = 1;
 
 function convertChoice(choice) {
   if (choice == 1) {
-    choice = "rock";
+    return "rock";
   } else if (choice == 2) {
-    choice = "paper";
+    return "paper";
   } else if (choice == 3) {
-    choice = "scissor";
+    return "scissor";
   } else {
-    choice = "Invalid input";
+    return "Invalid input";
   }
-  return choice;
 }
 
 function getComputerChoice() {
@@ -19,88 +19,47 @@ function getComputerChoice() {
   return convertChoice(choice);
 }
 
-function getHumanChoice() {
-  let choice = prompt(
-    "1 = Rock \n2 = Paper \n3 = Scissor \n\nChoose: 1, 2, or 3?"
-  );
-
-  return convertChoice(choice);
-}
-
-function playRound(humanChoice, computerChoice) {
-  let draw = "It's a draw! ";
-  let computerWins = "Computer wins. ";
-  let humanWins = "You win. ";
-  let invalidRound = "Invalid Round ";
+function playRound(humanChoice) {
+  const computerChoice = getComputerChoice();
+  const resultBox = document.getElementById('result-box');
+  resultBox.innerHTML = `You chose: ${humanChoice}<br>Computer chose: ${computerChoice}<br>`;
 
   if (humanChoice == computerChoice) {
-    console.log(draw);
+    resultBox.innerHTML += "It's a draw!";
+  } else if ((humanChoice == "rock" && computerChoice == "scissor") ||
+             (humanChoice == "paper" && computerChoice == "rock") ||
+             (humanChoice == "scissor" && computerChoice == "paper")) {
+    humanScore++;
+    resultBox.innerHTML += "You win this round!";
+  } else {
+    computerScore++;
+    resultBox.innerHTML += "Computer wins this round!";
   }
 
-  if (humanChoice == "rock") {
-    if (computerChoice == "paper") {
-      computerScore++;
-      console.log(computerWins + "You: " + humanScore + "    Computer: " + computerScore);
-    }
-  }
+  updateScores();
+  checkWinner();
+}
 
-  if (humanChoice == "paper") {
-    if (computerChoice == "rock") {
-      humanScore++;
-      console.log(humanWins + "You: " + humanScore + "    Computer: " + computerScore);
-    }
-  }
+function updateScores() {
+  document.getElementById('human-score').innerText = humanScore;
+  document.getElementById('computer-score').innerText = computerScore;
+  document.getElementById('round').innerText = round++;
+}
 
-  if (humanChoice == "rock") {
-    if (computerChoice == "scissor") {
-      humanScore++;
-      console.log(humanWins + "You: " + humanScore + "    Computer: " + computerScore);
-    }
-  }
-
-  if (humanChoice == "scissor") {
-    if (computerChoice == "rock") {
-      computerScore++;
-      console.log(computerWins + "You: " + humanScore + "    Computer: " + computerScore);
-    }
-  }
-
-  if (humanChoice == "paper") {
-    if (computerChoice == "scissor") {
-      computerScore++;
-      console.log(computerWins + "You: " + humanScore + "    Computer: " + computerScore);
-    }
-  }
-
-  if (humanChoice == "scissor") {
-    if (computerChoice == "paper") {
-      humanScore++;
-      console.log(humanWins + "You: " + humanScore + "    Computer: " + computerScore);
-    }
-  }
-
-  if (humanChoice == "Invalid input" || computerChoice == "Invalid input") {
-    console.log(invalidRound);
+function checkWinner() {
+  if (humanScore === 5) {
+    alert("Congrats, you win the game!");
+    resetGame();
+  } else if (computerScore === 5) {
+    alert("The computer beat you :(");
+    resetGame();
   }
 }
 
-function playGame() {
-  for (i = 1; i < 6; i++) {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    console.log("ROUND " + i);
-    console.log("You: " + humanChoice);
-    console.log("Computer: " + computerChoice);
-    playRound(humanChoice, computerChoice);
-  }
-  console.log("SCORE: \n\nYou: " + humanScore + "\nComputer: " + computerScore);
-  if (humanScore > computerScore) {
-    console.log("Congrats you win!");
-  } else if (computerScore > humanScore) {
-    console.log("The computer beat you :(");
-  } else if (humanScore == computerScore) {
-    console.log("Whelp, it's a tie.");
-  }
+function resetGame() {
+  humanScore = 0;
+  computerScore = 0;
+  round = 1;
+  updateScores();
+  document.getElementById('result-box').innerHTML = "";
 }
-
-playGame();
